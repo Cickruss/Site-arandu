2451deca9099158d5a2bebf06197c973b2a570a2
Cickruss <icaro.o.macedo@gmail.com>
2023-08-17 18:02:51 -0300

    Implementado nav bar e ajustes mobile

---
+++ src/script.js
@@ -0,0 +1,83 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    const container = document.querySelector('.container')
+    const inicioPagina = document.querySelector('.paginaLogo')
+    const sessoes = document.querySelectorAll('.containers');
+    const menus = document.querySelectorAll('.menu');
+    const textos = document.querySelectorAll('.textoMenu');
+    const classeAnima = 'atual';
+    const animatexto = 'animatexto';
+
+    function animaScroll() {
+        const topoPagina = window.pageYOffset + (window.innerHeight / 3);
+        if (topoPagina > inicioPagina.offsetTop) {
+            container.classList.remove('escondido');
+            menus.forEach(menu => menu.classList.remove(classeAnima));
+            textos.forEach(texto => texto.classList.remove(animatexto));
+
+        }
+        for (let i = 0; i < sessoes.length; i++) {
+            
+            if (topoPagina > sessoes[i].offsetTop) {
+                container.classList.add('escondido');
+                menus.forEach(menu => menu.classList.remove(classeAnima));
+                textos.forEach(texto => texto.classList.remove(animatexto));
+                menus[i].classList.add(classeAnima);
+                textos[i].classList.add(animatexto);
+
+            }
+        }
+    }
+
+    window.addEventListener('scroll', function () {
+        animaScroll();
+    });
+
+    animaScroll();
+});
+
+
+
+
+
+function scrollToSection(sectionId) {
+    const section = document.querySelector(sectionId);
+
+    if (section) {
+        const offsetTop = section.offsetTop;
+        window.scrollTo({
+            top: offsetTop,
+            behavior: 'smooth'
+        });
+    }
+}
+
+const linksNavegacao = document.querySelectorAll('.menu');
+
+linksNavegacao.forEach(link => {
+    link.addEventListener('click', function(event) {
+        event.preventDefault();
+        const sectionId = link.getAttribute('href');
+        scrollToSection(sectionId);
+    });
+});
+
+
+
+const btnTopo = document.getElementById('btnTopo');
+
+btnTopo.addEventListener('click', function() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
+
+window.addEventListener('scroll', function() {
+    if (window.pageYOffset > 300) {
+        btnTopo.classList.add('show');
+    } else {
+        btnTopo.classList.remove('show');
+    }
+});
+
